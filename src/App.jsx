41c469f707ff55db9b1bd5ae3a626e8b880e7698
Meomonsieur <phandangquan0212@gmail.com>
2025-05-20 01:22:46 +0700

    Refactor Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,11 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
-import { useColorScheme} from '@mui/material/styles'
-//import { useMediaQuery } from '@mui/material/useMediaQuery'
+import Board from './pages/Boards/_id'
 
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import Box from '@mui/material/Box'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeOulinedIcon from '@mui/icons-material/DarkModeOutlined'
-import SettingsBrightnessicon from '@mui/icons-material/SettingsBrightness'
-
-function ModeSelect() {
-  const { mode, setMode } = useColorScheme();
-  const handleChange = (event) => {
-    const selectedMode = event.target.value;
-    setMode(selectedMode);
-  };
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-      <Select
-        labelId="label-select-dark-light-mode"
-        id="select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-        <MenuItem value="light">
-        <div style={{display: 'flex', alignItems: 'center', gap: '5px'}}>
-        <LightModeIcon fontSize="small" /> Light
-        </div>
-        </MenuItem>
-        <MenuItem value="dark">
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-            <DarkModeOulinedIcon fontSize="small" /> Dark
-          </Box>
-        </MenuItem>
-        <MenuItem value="system">
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-            <SettingsBrightnessicon fontSize="small" /> System
-          </Box>
-        </MenuItem>
-      </Select>
-    </FormControl>
-  );
-}
-
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme();
-  // const preferDakMode = useMediaQuery('(prefers-color-scheme: dark)')
-  // const preferLightMode = useMediaQuery('(prefers-color-scheme: light)')
-
-  // console.log('preferDakMode', preferDakMode)
-  // console.log('preferLightMode', preferLightMode)
-
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-        localStorage.setItem('trello-dark-light-mode')
-        localStorage.getItem('trello-dark-light-mode')
-
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
 
 function App() {
   return (
-    <>
-    <ModeSelect />
-    <hr />
-    <ModeToggle />
-    <hr />
-
-    <Typography variant="h2" color="text.secondary">Hello world </Typography>
-    <Button variant="text">Text</Button>
-    <Button variant="contained">Contained</Button>
-    <Button variant="outlined">Outlined</Button>
-    
-    <br />
-    <AccessAlarmIcon />
-    <ThreeDRotation />
-    <HomeIcon color="primary" />
-    <HomeIcon color="secondary" />
-    <HomeIcon color="success" />
-    <HomeIcon color="action" />
-    <HomeIcon color="disabled" />
-    <HomeIcon sx={{ color: pink[500] }} />
-    </>
+  <>
+    <Board />
+  </>
   )
 }
 
